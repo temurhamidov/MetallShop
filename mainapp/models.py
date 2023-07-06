94b17a4b42d410e556c1e2e2d4a39531853d90ab
temurhamidov <temur20012007@gmail.com>
2023-07-06 08:40:39 +0500

    Model qismi yozildi

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class BaseModel(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(BaseModel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
+    class Meta:
+        verbose_name = 'Kategoriya'
+        verbose_name_plural = 'Kategoriyalar'
+
+
+class Type(BaseModel):
+    image = models.ImageField(upload_to='category/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    @property
+    def image_url(self):
+        if self.image:
+            return self.image.url
+        return "Image is not defined"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    size = models.IntegerField(null=True)
+    manfacturer = models.CharField(max_length=100, null=True)
+    theory = models.IntegerField(null=True)
+    unit = models.CharField(max_length=20, null=True)
+    price_cash = models.IntegerField(null=True)
+    price_cashless = models.IntegerField(null=True)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
